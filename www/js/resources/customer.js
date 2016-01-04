4cfd4871386bd6e5d5354ded9904c7de1bad7dc1
Yasmany Cubela Medina <yasmanycm@gmail.com>
2016-01-04 17:52:32 -0430

    updating variables

--- www/js/resources/customer.js
+++ www/js/resources/customer.js
@@ -1,25 +1,26 @@
-angular.module('app.resources', []).factory('customer', function ($resource, SERVER_CONF, userDatastore) {
-    return $resource(SERVER_CONF.API_HOST + 'customers/:customer', {customer: '@customer'}, {
-        confirm: {
-            method: 'POST',
-            url: SERVER_CONF.API_HOST + 'customers/:customer/confirms/:confirmation_code',
-            params: {customer: '@customer', confirmation_code: '@confirmationCode'}
-        },
-        requestAccessToken: {
-            method: 'GET',
-            url: SERVER_CONF.OAUTH_HOST + 'token',
-            headers: {
-                username: userDatastore.getNumber,
-                password: userDatastore.getPassword
+angular.module('app.resources', [])
+    .factory('customer', function ($resource, SERVER_CONF, OAUTH_CONF, userDatastore) {
+        return $resource(SERVER_CONF.API_HOST + 'customers/:customer', {customer: '@customer'}, {
+            confirm: {
+                method: 'POST',
+                url: SERVER_CONF.API_HOST + 'customers/:customer/confirms/:confirmation_code',
+                params: {customer: '@customer', confirmation_code: '@confirmationCode'}
+            },
+            requestAccessToken: {
+                method: 'GET',
+                url: OAUTH_CONF.OAUTH_HOST + 'token',
+                headers: {
+                    username: userDatastore.getNumber,
+                    password: userDatastore.getPassword
+                }
+            },
+            refreshAccessToken: {
+                method: 'GET',
+                url: OAUTH_CONF.OAUTH_HOST + 'token',
+                headers: {
+                    username: userDatastore.getNumber,
+                    password: userDatastore.getPassword
+                }
             }
-        },
-        refreshAccessToken: {
-            method: 'GET',
-            url: SERVER_CONF.OAUTH_HOST + 'token',
-            headers: {
-                username: userDatastore.getNumber,
-                password: userDatastore.getPassword
-            }
-        }
+        });
     });
-});
