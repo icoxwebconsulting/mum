7283417c99cc5d2579997288280d095e936e7b31
David De Freitas <davidjdr@gmail.com>
2016-02-17 10:40:29 -0400

    Indentación de services/contacts.js

--- www/js/services/contacts.js
+++ www/js/services/contacts.js
@@ -32,12 +32,12 @@ angular.module('app.contacts', []).factory('Contacts', function ($q, $rootScope)
 
         if (!ionic.Platform.isAndroid()) {
             //dummy data
-            var dummyName = ['David','Jesus','Ricardo'];
-            var dummyEmail = ['davidjdr@gmail.com', 'davidjdr+1@gmail.com','davidjdr+2@gmail.com'];
-            var dummyPhone = ['+584123600295','+584123600295','+584123600295'];
+            var dummyName = ['David', 'Jesus', 'Ricardo'];
+            var dummyEmail = ['davidjdr@gmail.com', 'davidjdr+1@gmail.com', 'davidjdr+2@gmail.com'];
+            var dummyPhone = ['+584123600295', '+584123600295', '+584123600295'];
             for (var i = 0, length = 3; i < length; i++) {
                 // create new contact
-                var contact = new Contact(dummyName[i],'img/account.png');
+                var contact = new Contact(dummyName[i], 'img/account.png');
                 contact.email = dummyEmail[i];
                 contact.phoneNumber = dummyPhone[i];
                 temContacts.push(contact);
@@ -51,51 +51,51 @@ angular.module('app.contacts', []).factory('Contacts', function ($q, $rootScope)
 
         } else {
 
-        var fields = [
-            navigator.contacts.fieldType.displayName,
-            navigator.contacts.fieldType.name,
-            navigator.contacts.fieldType.emails,
-            navigator.contacts.fieldType.phoneNumbers,
-            navigator.contacts.fieldType.photos
-        ];
+            var fields = [
+                navigator.contacts.fieldType.displayName,
+                navigator.contacts.fieldType.name,
+                navigator.contacts.fieldType.emails,
+                navigator.contacts.fieldType.phoneNumbers,
+                navigator.contacts.fieldType.photos
+            ];
 
-        var options = new ContactFindOptions();
-        options.multiple = true;
-        options.hasPhoneNumber = false;
+            var options = new ContactFindOptions();
+            options.multiple = true;
+            options.hasPhoneNumber = false;
 
-        navigator.contacts.find(fields,
-            function onSuccess(loadedContacts) {
-                for (var i = 0, length = loadedContacts.length; i < length; i++) {
-                    var loadedContact = loadedContacts[i];
-                    // only those hwo has display name
-                    if (loadedContact.displayName) {
-                        // create new contact
-                        var contact = new Contact(loadedContact.displayName,
-                            (loadedContact.photos) ? loadedContact.photos[0].value : 'img/account.png');
+            navigator.contacts.find(fields,
+                function onSuccess(loadedContacts) {
+                    for (var i = 0, length = loadedContacts.length; i < length; i++) {
+                        var loadedContact = loadedContacts[i];
+                        // only those hwo has display name
+                        if (loadedContact.displayName) {
+                            // create new contact
+                            var contact = new Contact(loadedContact.displayName,
+                                (loadedContact.photos) ? loadedContact.photos[0].value : 'img/account.png');
 
-                        if (loadedContact.emails) {
-                            contact.email = loadedContact.emails[0].value;
-                        } else {
-                            contact.email = null;
+                            if (loadedContact.emails) {
+                                contact.email = loadedContact.emails[0].value;
+                            } else {
+                                contact.email = null;
+                            }
+
+                            if (loadedContact.phoneNumbers) {
+                                contact.phoneNumber = loadedContact.phoneNumbers[0].value;
+                            } else {
+                                contact.phoneNumber = null;
+                            }
+
+                            temContacts.push(contact);
                         }
-
-                        if (loadedContact.phoneNumbers) {
-                            contact.phoneNumber = loadedContact.phoneNumbers[0].value;
-                        } else {
-                            contact.phoneNumber = null;
-                        }
-
-                        temContacts.push(contact);
                     }
-                }
-                temContacts.sort();
-                contacts = temContacts;
-                loading = false;
-                $rootScope.$emit('notifying-contact-loaded');
-                deferred.resolve(contacts);
-            }, function onError(error) {
-                deferred.reject(error);
-            }, options);
+                    temContacts.sort();
+                    contacts = temContacts;
+                    loading = false;
+                    $rootScope.$emit('notifying-contact-loaded');
+                    deferred.resolve(contacts);
+                }, function onError(error) {
+                    deferred.reject(error);
+                }, options);
 
         }
 
@@ -109,17 +109,17 @@ angular.module('app.contacts', []).factory('Contacts', function ($q, $rootScope)
             return loadContacts();
         } else {
 
-        if (contacts !== null && loading === false) {
-            deferred.resolve(contacts);
-        } else if (contacts === false && loading === false) {
-            return loadContacts();
-        } else {
-            $rootScope.$on('notifying-contact-loaded', function () {
+            if (contacts !== null && loading === false) {
                 deferred.resolve(contacts);
-            });
-        }
+            } else if (contacts === false && loading === false) {
+                return loadContacts();
+            } else {
+                $rootScope.$on('notifying-contact-loaded', function () {
+                    deferred.resolve(contacts);
+                });
+            }
 
-        return deferred.promise;
+            return deferred.promise;
 
         }
     }
