473de707c8745e1ecbfa2651c98beef1928a9381
nestor91sanchez <sanchez91nestor@gmail.com>
2017-06-22 10:50:26 -0400

    modified schedule

--- www/js/controllers/schedule.js
+++ www/js/controllers/schedule.js
@@ -1,123 +1,27 @@
-angular.module('app').controller('ScheduleCtrl', function ($scope, $state, $ionicPopup, messageService, $ionicScrollDelegate, $ionicPosition, userDatastore, DATETIME_FORMAT_CONF, messageStorage) {
+angular.module('app').controller('ScheduleCtrl', function ($scope, $state, $ionicPopup, messageService, $ionicScrollDelegate, $ionicPosition) {
 
-    function _removeTime(date) {
-        return date.day(1).hour(1).minute(1).second(1).millisecond(1);
-    }
-
-    function _buildMonth($scope, start, month) {
-        messageStorage.getScheduledMessagesCountByRange(start.clone().subtract(1, "week"), start.clone().add(2, "month"))
-            .then(function (scheduledMessages) {
-                $scope.weeks = [];
-                var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
-                while (!done) {
-                    $scope.weeks.push({days: _buildWeek(date.clone(), month, scheduledMessages)});
-                    date.add(1, "w");
-                    done = count++ > 2 && monthIndex !== date.month();
-                    monthIndex = date.month();
-                }
-            });
-    }
-
-    function _buildWeek(date, month, scheduledMessages) {
-        var days = [];
-        for (var i = 0; i < 7; i++) {
-            var hasEvents = false;
-
-            for (var j = 0, length = scheduledMessages.length; j < length; j++) {
-                if (scheduledMessages[j].at.substring(0, 10) == date.format('YYYY-MM-DD')) {
-                    hasEvents = true;
-                    break;
-                }
-            }
-
-            var day = {
-                name: date.format("dd").substring(0, 1),
-                number: date.date(),
-                isCurrentMonth: date.month() === month.month(),
-                isToday: date.isSame(new Date(), "day"),
-                date: date,
-                hasEvents: hasEvents
-            };
-            days.push(day);
-            date = date.clone();
-            date.add(1, "d");
-        }
-        return days;
-    }
-
-    function getAllScheduledMessages(start) {
-        messageStorage.getScheduledMessagesCountByRange(start.clone().subtract(1, "week"), start.clone().add(12, "month"))
-            .then(function (scheduledMessages) {
-                userDatastore.setScheduleMessages(scheduledMessages.length);
-            });
-    }
+    $scope.fecha = moment().add(10, 'minutes');
+    $scope.selectedTime = $scope.fecha.format('hh:mm a');
 
     var dateSchedule = userDatastore.getScheduleDay();
-    if(dateSchedule){
-        $scope.selected = _removeTime( moment(dateSchedule));
-    }else {
-        $scope.selected = _removeTime(moment());
-    }
-    $scope.month = $scope.selected.clone();
-
-    var start = $scope.selected.clone();
-    start.date(1);
-    _removeTime(start.day(0));
-
-    _buildMonth($scope, start, $scope.month);
-    getAllScheduledMessages(start);
-    $scope.totalScheduleMessages = userDatastore.getScheduleMessages();
-    console.log('$scope->>', $scope);
-    console.log('start->>', start);
-    console.log('$scope.month->>', $scope.month);
-
-    $scope.next = function () {
-        var next = $scope.month.clone();
-        _removeTime(next.month(next.month() + 1).date(1));
-        $scope.month.month($scope.month.month() + 1);
-        _buildMonth($scope, next, $scope.month);
-    };
-
-    $scope.previous = function () {
-        var previous = $scope.month.clone();
-        _removeTime(previous.month(previous.month() - 1).date(1));
-        $scope.month.month($scope.month.month() - 1);
-        _buildMonth($scope, previous, $scope.month);
-    };
-
     if (dateSchedule){
         $scope.fecha = moment(dateSchedule);
-        $scope.selectedTime = $scope.fecha.format('HH:mm');
-    } else {
-        $scope.fecha = moment().add(10, 'minutes');
-        $scope.selectedTime = $scope.fecha.format('HH:mm');
+        $scope.selectedTime = $scope.fecha.format('hh:mm a');
     }
-
-    $scope.$on('$ionicView.beforeEnter', function (e) {
-        userDatastore.setStateCurrentName($state.current.name);
-    });
+    else {
+        $scope.fecha = moment().add(10, 'minutes');         $scope.selectedTime = $scope.fecha.format('hh:mm a');
+    }
 
     $scope.$on('$ionicView.enter', function () {
         //fechas
-//        $scope.fecha = moment().add(10, 'minutes');
-//        $scope.selectedTime = $scope.fecha.format('HH:mm');
-//        dateSchedule();
-
-        var dateSchedule = userDatastore.getScheduleDay();
-        if (dateSchedule){
-            $scope.fecha = moment(dateSchedule);
-            $scope.selectedTime = $scope.fecha.format('HH:mm');
-        } else {
-            $scope.fecha = moment().add(10, 'minutes');
-            $scope.selectedTime = $scope.fecha.format('HH:mm');
-        }
+        $scope.fecha = moment().add(10, 'minutes');
+        $scope.selectedTime = $scope.fecha.format('hh:mm a');
         $scope.scrollToDay();
     });
 
     $scope.scrollToDay = function () {
         var element = $ionicPosition.position(angular.element(document.getElementsByClassName('calendar-day clear-btn flex-xs active')[0]));
-//        console.log('scroll to day', $ionicPosition.position(angular.element(document.getElementsByClassName('calendar-day clear-btn flex-xs active'))));
-        console.info('element --> ', angular.element(document.getElementsByClassName('calendar-day clear-btn flex-xs active')[0]));
+        console.info('element --> ', element);
         $ionicScrollDelegate.$getByHandle('vscroll').scrollTo(element.left, element.top, true);
     };
 
@@ -141,39 +45,17 @@ angular.module('app').controller('ScheduleCtrl', function ($scope, $state, $ioni
         }
     };
 
-    $scope.select = function (day) {
-        $scope.selected = day.date;
-        var dateParam = moment((day.date._d).toString()).format('YYYY-MM-DD');
-        userDatastore.setScheduleDay(dateParam);
-        var selectedDate = moment($scope.selected).format(DATETIME_FORMAT_CONF.dateTimeFormat);
-        var selected = moment($scope.selected).format('D');
-
-        console.info('$scope.selected', moment($scope.selected).format(DATETIME_FORMAT_CONF.dateTimeFormat));
-        console.info('moment()', moment().format(DATETIME_FORMAT_CONF.dateTimeFormat));
-
-        if (selectedDate < moment().format(DATETIME_FORMAT_CONF.dateTimeFormat) ){
-            showPopup();
-        }else {
-            $scope.fecha.date(selected);
-        }
-    };
-
     $scope.changeDay = function ($index) {
         var selected = $index + 1;
-        var selectedDate = moment($scope.fecha.date(selected)).format(DATETIME_FORMAT_CONF.dateTimeFormat);
-
-        console.info('selectedDate', selectedDate);
-
-        if (selectedDate < moment().format(DATETIME_FORMAT_CONF.dateTimeFormat)) {
+        if (selected < moment().date() && $scope.fecha.month() <= moment().month()) {
             showPopup();
         } else {
             $scope.fecha.date(selected);
-            console.info('$scope.fecha.date(selected)', $scope.fecha.date(selected));
         }
     };
 
     $scope.getTime = function () {
-        return $scope.fecha.format('HH:m');
+        return $scope.fecha.format('hh:m a');
     };
 
     $scope.monthUp = function () {
@@ -181,17 +63,21 @@ angular.module('app').controller('ScheduleCtrl', function ($scope, $state, $ioni
     };
 
     $scope.monthDown = function () {
-      $scope.fecha.subtract(1, 'M');
+        if ($scope.fecha.month() <= moment().month()) {
+            showPopup();
+        } else {
+            $scope.fecha.subtract(1, 'M');
+        }
     };
 
     $scope.timeUp = function () {
         $scope.fecha.add(1, 'm');
-        $scope.selectedTime = $scope.fecha.format('HH:mm');
+        $scope.selectedTime = $scope.fecha.format('hh:mm a');
     };
 
     $scope.timeDown = function () {
         $scope.fecha.subtract(1, 'm');
-        $scope.selectedTime = $scope.fecha.format('HH:mm');
+        $scope.selectedTime = $scope.fecha.format('hh:mm a');
     };
 
     $scope.getMaxDay = function () {
@@ -214,14 +100,14 @@ angular.module('app').controller('ScheduleCtrl', function ($scope, $state, $ioni
             var selectedTime = new Date(val * 1000);
             $scope.fecha.hour(selectedTime.getUTCHours());
             $scope.fecha.minute(selectedTime.getUTCMinutes());
-            $scope.selectedTime = $scope.fecha.format('HH:mm');
+            $scope.selectedTime = $scope.fecha.format('hh:mm a');
         }
     }
 
     $scope.timePickerObject = {
         inputEpochTime: ($scope.fecha.unix() + ($scope.fecha.utcOffset() * 60)),  //Optional
         step: 1,  //Optional
-        format: 24,  //Optional
+        format: 12,  //Optional
         titleLabel: 'Seleccione una hora',  //Optional
         setLabel: 'Aceptar',  //Optional
         closeLabel: 'Cancelar',  //Optional
